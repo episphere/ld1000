fd09a5f0ee9cded632f73557d003709740d3167f
Jonas Almeida <jalmeida@mathbiol.org>
2023-04-20 09:13:06 -0400

    resuming detachment of SDK from UI

--- jonas/index.js
+++ jonas/index.js
@@ -2,6 +2,6 @@ console.log(`${Date()}\n${location.origin+location.pathname}index.js loaded`);
 
 (async function(){
     VCF = await import('./export.js')
-    //VCF.UI()
-    VCF.LDextraction(chrpos1='chr7:16876630',chrpos2='chr7:16863828')
+    VCF.UI()
+    //VCF.LDextraction(chrpos1='chr7:16876630',chrpos2='chr7:16863828')
 })()
